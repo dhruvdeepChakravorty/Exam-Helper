570e61a2ee1df771e346cea0187c2110fa79d8b7
Dhruvdeep Chakravorty <dhruvdeepchakravorty17@gmail.com>
2026-04-16 21:59:09 +0530

    feat(upload): enhance upload functionality with additional fields and validation

--- server/src/types/url.types.ts
+++ server/src/types/url.types.ts
@@ -1,13 +1,18 @@
 import { z } from "zod";
 
 export const urlSchema = z.object({
-  fileName: z.array(z.string()).max(10,"Maximum 10 files allowed"),
-  fileType: z.array(z.enum(["application/pdf", "image/jpeg", "image/png"])).max(10),
+  fileName: z.array(z.string()).max(10, "Maximum 10 files allowed"),
+  fileType: z
+    .array(z.enum(["application/pdf", "image/jpeg", "image/png"]))
+    .max(10),
   fileHashes: z.array(z.string()).max(10),
 });
 
 export const confirmSchema = z.object({
   fileKeys: z.array(z.string()),
-  fileHashes:z.array(z.string()),
+  fileHashes: z.array(z.string()),
   difficulty: z.enum(["easy", "medium", "hard"]),
+  subject: z.string(),
+  educationLevel: z.enum(["school", "college"]),
+  year: z.string(),
 });
